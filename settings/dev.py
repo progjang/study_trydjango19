57f968366ee51addad90bfb9460bf65f4619f727
S Y Chang <wooritobi@gmail.com>
2018-08-03 00:34:36 +0900

    settings/dev.py 에 static, media 서빙 룰 추가

--- settings/dev.py
+++ settings/dev.py
@@ -2,3 +2,12 @@ from .common import *
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "settings", "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
